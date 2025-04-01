d27fbeadf4394ce666b928dd8cb07918323a9bf4
daniels cai <danxcai@gmail.com>
2025-04-01 09:20:01 +0800

    优化

--- app/installation/page.tsx
+++ app/installation/page.tsx
@@ -1,32 +1,20 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import InstallationLayout from './components/InstallationLayout';
-import InstallWizard from './components/InstallWizard';
+
 
 export default function InstallationPage() {
     const router = useRouter();
-    const [isWizardOpen, setIsWizardOpen] = useState(true);
 
-    const handleClose = () => {
-        setIsWizardOpen(false);
-        router.back();
-    };
-
-    const handleComplete = (data: any) => {
-        console.log('Installation completed:', data);
-        setIsWizardOpen(false);
-        router.back();
-    };
+    useEffect(() => {
+        router.push('/installation/servers');
+    }, [router]);
 
     return (
         <InstallationLayout>
-            <InstallWizard 
-                isOpen={isWizardOpen}
-                onClose={handleClose}
-                onComplete={handleComplete}
-            />
+            {null}
         </InstallationLayout>
     );
 }
